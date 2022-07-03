54e30f544db428891078736303b5684773c64128
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2022-07-03 13:17:28 -0300

    Added force level and template checking

--- README.md
+++ README.md
@@ -14,11 +14,18 @@ This repository is for personal use. It is not a general purpose tool, be aware 
 ## Usage
 
 ```bash
-genc <number of files> [template]
+genc <number of files>[force] [template]
 ```
 
 * `number of lines` (REQUIRED): The number of files to generate;
 
-* `template` (OPTIONAL): The base template to be used from the [`templates/`](templates) directory, if not specified, the [`default`](templates/default.c) template will be used.
+* `force` (OPTIONAL): The level of force applied to the current working directory. There are four levels:
 
-> **Important:** The script will **OVERWRITE** existing files, be careful!
+    | Level | Representation | Description                                                                     | Default |
+    | ----- | -------------- | ------------------------------------------------------------------------------- | ------- |
+    | 0     |                | No files will be overwritten if encountered                                     | Yes     |
+    | 1     | `!`            | Only C source files will be overwritten if encountered                          | No      |
+    | 2     | `!!`           | C source files and the Makefile will be overwritten if encountered              | No      |
+    | 3     | `!!!`          | The directory C source files and Makefile will be deleted before the generation | No      |
+
+* `template` (OPTIONAL): The base template to be used from the [`templates/`](templates) directory, if not specified, the [`default`](templates/default.c) template will be used.
--- generate.sh
+++ generate.sh
@@ -4,14 +4,14 @@ SCRIPT_PATH=$(dirname $(realpath $0))
 OS=$(lsb_release -sd)
 
 # Variables
-ACTIVITIES=$1
+TARGET=$1
 TEMPLATE=$2
 
 # Polyfill
-if [[ -z $ACTIVITIES ]]
+if [[ -z $TARGET ]]
 then
-    echo -n "Quantity of activities: "
-    read ACTIVITIES
+    echo -n "Quantity: "
+    read TARGET
 fi
 
 if [[ -z $TEMPLATE ]]
@@ -19,14 +19,47 @@ then
     TEMPLATE="default"
 fi
 
+# Work variables
+QUANTITY=$(echo $TARGET | grep '[0-9]*' -o)
+FORCE=$(echo $TARGET | grep '!' -o | wc -l)
 TEMPLATE_FILE=$SCRIPT_PATH/templates/$TEMPLATE.c
 MAKEFILE_FILE=$SCRIPT_PATH/Makefile
 
-# Generate files
+# Check template files
+if [[ ! -f $MAKEFILE_FILE ]]
+then
+    echo "Makefile file not found"
+    exit 1
+fi
 
-for i in $(seq $ACTIVITIES)
+if [[ ! -f $TEMPLATE_FILE ]]
+then
+    echo "Template file not found"
+    exit 1
+fi
+
+# Cleanup files
+if [[ $FORCE -ge 3 ]]
+then
+    rm -f $CWD/*.c $CWD/Makefile
+fi
+
+# Generate files
+for i in $(seq $QUANTITY)
 do
-    cat $TEMPLATE_FILE | awk -v "iter=$i" -v "os=$OS" '{gsub(/\[\[ITER\]\]/,iter);gsub(/\[\[OS\]\]/,os)}1' > $CWD/$i.c
+    FILENAME=$CWD/$i.c
+    if [[ -f $FILENAME && $FORCE -lt 1 ]]
+    then
+        echo "File $FILENAME already exists, skipping"
+    else
+        cat $TEMPLATE_FILE | awk -v "iter=$i" -v "os=$OS" '{gsub(/\[\[ITER\]\]/,iter);gsub(/\[\[OS\]\]/,os)}1' > $FILENAME
+    fi
 done
 
-cat $MAKEFILE_FILE | awk -v "files=$(seq -s ' ' $ACTIVITIES)" '{gsub(/\[\[FILES\]\]/,files)}1' > $CWD/Makefile
+FILENAME=$CWD/Makefile
+if [[ -f $FILENAME && $FORCE -lt 2 ]]
+then
+    echo "File $FILENAME already exists, skipping"
+else
+    cat $MAKEFILE_FILE | awk -v "files=$(seq -s ' ' $QUANTITY)" '{gsub(/\[\[FILES\]\]/,files)}1' > $CWD/Makefile
+fi
